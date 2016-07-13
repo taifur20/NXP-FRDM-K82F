7eb4aa9068ef4e683554bb1f06a9f1a66a8f5d87
Md. Khairul Alam <khairul.uapstu@gmail.com>
2016-07-13 18:41:02 +0600

    FlexIO based smart helmet complete source code
    Code was developed using Kinetis Design Studio & Kinetis SDK

---
+++ FlexIO_Smart_helmet/kds/debug/board/board.d
@@ -0,0 +1,53 @@
+board/board.o: \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/boards/frdmk82f/user_apps/smart_helmet/board.c \
+ ../../../../../../devices/MK82F25615/drivers/fsl_common.h \
+ ../../../../../../devices/MK82F25615/fsl_device_registers.h \
+ ../../../../../../devices/MK82F25615/MK82F25615.h \
+ ../../../../../../CMSIS/Include/core_cm4.h \
+ ../../../../../../CMSIS/Include/core_cmInstr.h \
+ ../../../../../../CMSIS/Include/cmsis_gcc.h \
+ ../../../../../../CMSIS/Include/core_cmFunc.h \
+ ../../../../../../CMSIS/Include/core_cmSimd.h \
+ ../../../../../../devices/MK82F25615/system_MK82F25615.h \
+ ../../../../../../devices/MK82F25615/MK82F25615_features.h \
+ ../../../../../../devices/MK82F25615/drivers/fsl_clock.h \
+ ../../../../../../devices/MK82F25615/drivers/fsl_port.h \
+ ../../../../../../devices/MK82F25615/drivers/fsl_common.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/boards/frdmk82f/user_apps/smart_helmet/clock_config.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/boards/frdmk82f/user_apps/smart_helmet/board.h \
+ ../../../../../../devices/MK82F25615/drivers/fsl_gpio.h \
+ ../../../../../../devices/MK82F25615/utilities/fsl_debug_console.h
+
+../../../../../../devices/MK82F25615/drivers/fsl_common.h:
+
+../../../../../../devices/MK82F25615/fsl_device_registers.h:
+
+../../../../../../devices/MK82F25615/MK82F25615.h:
+
+../../../../../../CMSIS/Include/core_cm4.h:
+
+../../../../../../CMSIS/Include/core_cmInstr.h:
+
+../../../../../../CMSIS/Include/cmsis_gcc.h:
+
+../../../../../../CMSIS/Include/core_cmFunc.h:
+
+../../../../../../CMSIS/Include/core_cmSimd.h:
+
+../../../../../../devices/MK82F25615/system_MK82F25615.h:
+
+../../../../../../devices/MK82F25615/MK82F25615_features.h:
+
+../../../../../../devices/MK82F25615/drivers/fsl_clock.h:
+
+../../../../../../devices/MK82F25615/drivers/fsl_port.h:
+
+../../../../../../devices/MK82F25615/drivers/fsl_common.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/boards/frdmk82f/user_apps/smart_helmet/clock_config.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/boards/frdmk82f/user_apps/smart_helmet/board.h:
+
+../../../../../../devices/MK82F25615/drivers/fsl_gpio.h:
+
+../../../../../../devices/MK82F25615/utilities/fsl_debug_console.h:
---
+++ FlexIO_Smart_helmet/kds/debug/drivers/fsl_ltc_edma.d
@@ -0,0 +1,47 @@
+drivers/fsl_ltc_edma.o: \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_ltc_edma.c \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_ltc_edma.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_common.h \
+ ../../../../../../devices/MK82F25615/fsl_device_registers.h \
+ ../../../../../../devices/MK82F25615/MK82F25615.h \
+ ../../../../../../CMSIS/Include/core_cm4.h \
+ ../../../../../../CMSIS/Include/core_cmInstr.h \
+ ../../../../../../CMSIS/Include/cmsis_gcc.h \
+ ../../../../../../CMSIS/Include/core_cmFunc.h \
+ ../../../../../../CMSIS/Include/core_cmSimd.h \
+ ../../../../../../devices/MK82F25615/system_MK82F25615.h \
+ ../../../../../../devices/MK82F25615/MK82F25615_features.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_clock.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_ltc.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_dmamux.h \
+ C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_edma.h
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_ltc_edma.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_common.h:
+
+../../../../../../devices/MK82F25615/fsl_device_registers.h:
+
+../../../../../../devices/MK82F25615/MK82F25615.h:
+
+../../../../../../CMSIS/Include/core_cm4.h:
+
+../../../../../../CMSIS/Include/core_cmInstr.h:
+
+../../../../../../CMSIS/Include/cmsis_gcc.h:
+
+../../../../../../CMSIS/Include/core_cmFunc.h:
+
+../../../../../../CMSIS/Include/core_cmSimd.h:
+
+../../../../../../devices/MK82F25615/system_MK82F25615.h:
+
+../../../../../../devices/MK82F25615/MK82F25615_features.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_clock.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_ltc.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_dmamux.h:
+
+C:/Users/taifur/Desktop/nxp/FRDM_K82F/devices/MK82F25615/drivers/fsl_edma.h:
